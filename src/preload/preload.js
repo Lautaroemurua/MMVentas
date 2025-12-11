83ad5892228025201aceb434a27c090fcc8d4a88
Lautaro Emurua <lautaroemurua@gmail.com>
2025-12-10 21:41:31 -0300

    fix:modificacion de latest.yml

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -41,4 +41,5 @@ contextBridge.exposeInMainWorld('api', {
   // Producto-Modificadores
   obtenerModificadoresProducto: (productoId) => ipcRenderer.invoke('obtener-modificadores-producto', productoId),
   guardarModificadoresProducto: (data) => ipcRenderer.invoke('guardar-modificadores-producto', data)
+  lkea
 });
